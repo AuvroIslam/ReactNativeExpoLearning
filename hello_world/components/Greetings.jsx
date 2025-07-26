57509f2adc0568ce29ef8b0bf8563e03aff49876
AuvroIslam <auvro2107024@stud.kuet.ac.bd>
2025-07-26 19:11:01 +0600

    customcomponents

---
+++ hello_world/components/Greetings.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+const Greetings = ({ name }) => {
+  return (
+    <View>
+      <Text>Greetings {name}</Text>
+    </View>
+  )
+}
+export default Greetings
